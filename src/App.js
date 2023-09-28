683d1105ebd46cea15a96a49b6b6301d8d69da05
Ceri <ceriwoolway@gmail.com>
2023-09-28 21:10:40 +0100

    Add google analytics

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { getFormattedAchievements } from './data.js';
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-2962N8TTQJ');
 
-const steamGameID = 1158850;
-const apiKey = '9C744478D34930318FB5C67B3613E409';
-
-const apiAddresses = {
-  achievementNames: `https://corsproxy.io/?http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v0002/?key=${apiKey}&appid=${steamGameID}&l=english&format=json`,
-  achievementPercentages: `https://corsproxy.io/?http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=${steamGameID}&format=json`,
-};
-
-async function fetchData(apiUrl) {
-  try {
-    const response = await fetch(apiUrl);
-
-    if (!response.ok) {
-      throw new Error(`Request failed with status ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    throw error;
-  }
-}
-
-// myThing();
-
-const formatAchievements = async () => {
-  const info = await fetchData(apiAddresses.achievementNames);
-  const percentages = await fetchData(apiAddresses.achievementPercentages);
-
-  const formattedData = info.game.availableGameStats.achievements.map((item, index) => {
-    return { ...item, percent: percentages.achievementpercentages.achievements[index].percent };
-  });
-
-  return formattedData;
-};
-
-const dataList = await formatAchievements();
+const dataList = await getFormattedAchievements();
 
 const AchievementList = ({ showHiddenAchievements }) => {
   console.log(showHiddenAchievements);
@@ -76,6 +42,10 @@ const AchievementCard = ({ achievement }) => {
 };
 
 function App() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname);
+  }, []);
+
   const [showHiddenAchievements, toggleHiddenAchievements] = useState(false);
 
   const toggleHidden = () => {
